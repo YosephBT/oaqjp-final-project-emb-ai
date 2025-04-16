fe6e58b5273a73e3af019f4f7c4611edc8ce599f
YosephBT <yosephbelay.t@gmail.com>
2025-04-15 20:36:43 -0400

    error handling

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def em_detector():
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    response = emotion_detector(text_to_analyze)
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    
+    if dominant_emotion is None:
+        return "Invalid input! Please try again."
+    else: 
+        return f'For the given statement, the system response is anger: {anger}, disgust: {disgust}, fear: {fear}, joy: {joy}, sadness: {sadness}, dominant_emotion: {dominant_emotion} '
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
